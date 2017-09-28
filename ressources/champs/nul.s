a666163ab866326954d1ae072a962f15841fa4c0
Adrien FOURCADE <afourcad@e3r5p10.42.fr>
2017-09-28 18:27:33 +0200

    ressources errase

--- ressources/champs/nul.s
+++
@@ -1,7 +0,0 @@
-.name "nul"
-.comment "nuuuul"
-
-deb:	ldi		%0, %0, r0
-		sti		r0, %:deb, %1
-live:	live	%1
-		zjmp	%:live
--- ressources/nul.s
+++
@@ -1,7 +0,0 @@
-.name "wqw"
-.comment "nuul"
-
-deb:	ldi		%0, %0, r0
-		sti		r0, %:deb, %1
-live:	live	%1
-		zjmp	%:live
