11c117ca8329686f72979586f6db786ce8aee4fa
Adrien FOURCADE <afourcad@e1r3p4.42.fr>
2017-10-05 19:30:42 +0200

    store and load change

--- corewar_dir/src/instructions/direct_load.c
+++ corewar_dir/src/instructions/direct_load.c
@@ -6,13 +6,37 @@
 /*   By: afourcad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/29 16:15:07 by afourcad          #+#    #+#             */
-/*   Updated: 2017/10/02 16:18:04 by afourcad         ###   ########.fr       */
+/*   Updated: 2017/10/05 19:20:31 by afourcad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "corewar.h"
 
-void	direct_load(int source, int *dest, int *carry, int pc)
+void	direct_load(t_vm *vm, t_proc *proc, t_instruction *inst)
 {
-	*dest = source;
-	*carry = *carry > 0 ? 0 : 1;
+	int	rel_pc;
+
+	rel_pc = 0;
+	if (inst->param_types[0] == IND_CODE)
+	{
+		rel_pc = (proc->pc + (inst->params[0] % IDX_MOD) + 2) % MEM_SIZE;
+		proc->r[inst->params[1]] = four_octets_to_int(vm->ram, rel_pc);
+	}
+	else
+		proc->r[inst->params[1]] = inst->params[0];
+	proc->carry = inst->params[0] == 0 ? 1 : 0;
+}
+
+void	long_direct_load(t_vm *vm, t_proc *proc, t_instruction *inst)
+{
+	int	rel_pc;
+
+	rel_pc = 0;
+	if (inst->param_types[0] == IND_CODE)
+	{
+		rel_pc = (proc->pc + inst->params[0] + 2) % MEM_SIZE;
+		proc->r[inst->params[1]] = four_octets_to_int(vm->ram, rel_pc);
+	}
+	else
+		proc->r[inst->params[1]] = inst->params[0];
+	proc->carry = inst->params[0] == 0 ? 1 : 0;
 }
