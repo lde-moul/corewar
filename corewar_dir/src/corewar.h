c2f04839925cbf8a69682101d6567dfbc7b1c4a0
afourcad <fourcade.adri2@gmail.com>
2017-10-08 14:45:33 +0200

    arf

--- corewar_dir/src/corewar.h
+++ corewar_dir/src/corewar.h
@@ -6,21 +6,21 @@
 /*   By: gdelabro <gdelabro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/26 20:10:14 by gdelabro          #+#    #+#             */
-/*   Updated: 2017/10/05 20:19:54 by lde-moul         ###   ########.fr       */
+/*   Updated: 2017/10/06 17:25:31 by lde-moul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COREWAR_H
 # define COREWAR_H
 
-#include "../../ft_printf/ft_printf.h"
-#include "../../op/op.h"
-#include <fcntl.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/time.h>
+# include "../../ft_printf/ft_printf.h"
+# include "../../op/op.h"
+# include <fcntl.h>
+# include <sys/types.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <sys/time.h>
 
 typedef struct	s_player
 {
@@ -29,7 +29,7 @@ typedef struct	s_player
 	t_header		header;
 	int				last_live;
 	int				nb_live;
-}					t_player;
+}				t_player;
 
 typedef struct	s_instruction
 {
@@ -38,7 +38,7 @@ typedef struct	s_instruction
 	int				ocp;
 	t_arg_type		param_types[3];
 	int				params[3];
-}					t_instruction;
+}				t_instruction;
 
 typedef struct	s_proc
 {
@@ -50,7 +50,7 @@ typedef struct	s_proc
 	int				last_live;
 	char			opcode;
 	struct s_proc	*next;
-}					t_proc;
+}				t_proc;
 
 typedef struct	s_vm
 {
@@ -69,19 +69,18 @@ void			kill_process(t_proc *process, t_vm *vm);
 void			execute_instruction(t_proc *process, t_vm *vm);
 
 void			parse(int argc, char **argv, t_vm *vm);
-void			parse(int argc, char **argv, t_vm *vm);
 void			load_player(t_player *p, const char *name);
 
 int				swap_int(int n);
 unsigned int	swap_uint(unsigned int n);
 short			swap_short(short n);
 
+short			two_octets_to_short(char ram[MEM_SIZE], int pc);
+int				four_octets_to_int(char ram[MEM_SIZE], int pc);
+
 /*
 ** Instructions
 */
-
-short			two_octets_to_short(char ram[MEM_SIZE], int pc);
-int				four_octets_to_int(char ram[MEM_SIZE], int pc);
 
 void			direct_load(t_vm *vm, t_proc *proc, t_instruction *inst);
 void			long_direct_load(t_vm *vm, t_proc *proc, t_instruction *inst);
@@ -94,5 +93,7 @@ void			cor_and(t_vm *vm, t_proc *proc, t_instruction *inst);
 void			cor_or(t_vm *vm, t_proc *proc, t_instruction *inst);
 void			cor_xor(t_vm *vm, t_proc *proc, t_instruction *inst);
 void			zjmp(short jump, int *pc, int carry);
+
+void			(*g_op_functions[16])(t_vm*, t_proc*, t_instruction*);
 
 #endif
