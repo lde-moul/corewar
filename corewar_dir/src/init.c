455035db3f45e27d27e7d4369b05c73ae0d07e78
Adrien FOURCADE <afourcad@e3r5p10.42.fr>
2017-10-12 19:05:56 +0200

    change param in init.c

--- corewar_dir/src/init.c
+++ corewar_dir/src/init.c
@@ -6,7 +6,7 @@
 /*   By: lde-moul <lde-moul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/02 17:17:26 by lde-moul          #+#    #+#             */
-/*   Updated: 2017/10/12 18:41:27 by lde-moul         ###   ########.fr       */
+/*   Updated: 2017/10/12 19:01:54 by afourcad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void	init_player(int n, t_vm *vm)
 	player = &vm->players[n];
 	pc = n * MEM_SIZE / vm->num_players;
 	ft_memcpy(vm->ram + pc, player->prog, player->header.prog_size);
-	ft_memset(vm->ram_color + pc, n + 1, player->header.prog_size);
+	ft_memset(vm->ram_color + pc, n + 2, player->header.prog_size);
 	create_process(vm, pc, player->number, 0);
 }
 
@@ -29,7 +29,7 @@ void		init_vm(t_vm *vm)
 	int	i;
 
 	ft_bzero(vm->ram, MEM_SIZE);
-	ft_bzero(vm->ram_color, MEM_SIZE);
+	ft_memset(vm->ram_color, 1, MEM_SIZE);
 	vm->processes = NULL;
 	i = 0;
 	while (i < vm->num_players)
--- corewar_dir/src/ncurses/init.c
+++ corewar_dir/src/ncurses/init.c
@@ -6,7 +6,7 @@
 /*   By: lde-moul <lde-moul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/11 17:19:42 by lde-moul          #+#    #+#             */
-/*   Updated: 2017/10/12 17:54:33 by afourcad         ###   ########.fr       */
+/*   Updated: 2017/10/12 19:03:29 by afourcad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ void	init_ncurses(t_vm *vm)
 	noecho();
 	start_color();
 	cbreak();
-	init_pair(0, COLOR_WHITE, COLOR_BLACK);
-	init_pair(1, COLOR_GREEN, COLOR_BLACK);
-	init_pair(2, COLOR_RED, COLOR_BLACK);
-	init_pair(3, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(4, COLOR_BLUE, COLOR_BLACK);
-	init_pair(5, COLOR_BLACK, COLOR_WHITE);
-	init_pair(6, COLOR_WHITE, COLOR_GREEN);
-	init_pair(7, COLOR_WHITE, COLOR_RED);
-	init_pair(8, COLOR_WHITE, COLOR_YELLOW);
-	init_pair(9, COLOR_WHITE, COLOR_BLUE);
+	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	init_pair(3, COLOR_RED, COLOR_BLACK);
+	init_pair(4, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(5, COLOR_BLUE, COLOR_BLACK);
+	init_pair(6, COLOR_BLACK, COLOR_WHITE);
+	init_pair(7, COLOR_WHITE, COLOR_GREEN);
+	init_pair(8, COLOR_WHITE, COLOR_RED);
+	init_pair(9, COLOR_WHITE, COLOR_YELLOW);
+	init_pair(10, COLOR_WHITE, COLOR_BLUE);
 }
