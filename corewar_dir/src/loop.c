d1714cad18407e045edf24f85992d2b34ceaab59
Louis-antoine DE MOULINS DE ROCHEFORT <lde-moul@e3r4p11.42.fr>
2017-10-12 19:17:34 +0200

    Fix with process handling

--- corewar_dir/src/loop.c
+++ corewar_dir/src/loop.c
@@ -6,7 +6,7 @@
 /*   By: lde-moul <lde-moul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/02 17:15:47 by lde-moul          #+#    #+#             */
-/*   Updated: 2017/10/12 16:37:25 by afourcad         ###   ########.fr       */
+/*   Updated: 2017/10/12 19:12:32 by lde-moul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ void		handle_main_loop(t_vm *vm)
 	while (i < 10000)
 	{
 		handle_processes(vm);
+		display_ram(vm);
 		getch();
 		i++;
 	}
