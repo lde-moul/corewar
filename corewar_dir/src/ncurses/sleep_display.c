0f35f44313db32924d994d181dbbaec129986b35
Louis-antoine DE MOULINS DE ROCHEFORT <lde-moul@e1r6p10.42.fr>
2017-10-20 17:20:28 +0200

    Fix speed changing again

--- corewar_dir/src/ncurses/sleep_display.c
+++ corewar_dir/src/ncurses/sleep_display.c
@@ -6,7 +6,7 @@
 /*   By: afourcad <afourcad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/18 17:42:12 by afourcad          #+#    #+#             */
-/*   Updated: 2017/10/20 16:40:51 by lde-moul         ###   ########.fr       */
+/*   Updated: 2017/10/20 16:45:28 by lde-moul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int	sleep_display(t_vm *vm)
 	else if (key == 27)
 		return (1);
 	else if (key == '=')
-		vm->speed = vm->speed - SPEED_STEP > MAX_SPEED ?
+		vm->speed = vm->speed > MAX_SPEED + SPEED_STEP ?
 			vm->speed - SPEED_STEP : MAX_SPEED;
 	else if (key == '-' && vm->speed < MIN_SPEED)
-		vm->speed = vm->speed + SPEED_STEP > MIN_SPEED ?
+		vm->speed = vm->speed < MIN_SPEED - SPEED_STEP ?
 			vm->speed + SPEED_STEP : MIN_SPEED;
 	else if (key == ']')
 		vm->speed = MAX_SPEED;
