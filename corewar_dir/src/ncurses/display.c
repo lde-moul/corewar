20e4a87d564ff40ce43be486783e2bc6e2eba5ba
Adrien FOURCADE <afourcad@e3r3p12.42.fr>
2017-10-25 19:29:34 +0200

    display nb_process

--- corewar_dir/src/ncurses/display.c
+++ corewar_dir/src/ncurses/display.c
@@ -6,7 +6,7 @@
 /*   By: afourcad <afourcad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/09 17:51:24 by afourcad          #+#    #+#             */
-/*   Updated: 2017/10/25 18:51:23 by afourcad         ###   ########.fr       */
+/*   Updated: 2017/10/25 18:57:42 by afourcad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,12 @@ void	display_info(t_vm *vm)
 	int		players;
 
 	players = 0;
-	line  = 7;
+	line  = 9;
 	info = subwin(stdscr, 66, 70, 0, 195);
 	box(info, ACS_VLINE, ACS_HLINE);
 	mvwprintw(info, 3, 3, "Current_cycle: %d", vm->cycle);
 	mvwprintw(info, 5, 3, "Current_live: %-10d", vm->num_lives);
+	mvwprintw(info, 7, 3, "Processes: %-10d", vm->num_processes);
 	while (players < vm->num_players)
 	{
 		mvwprintw(info, line, 3, "Player (%d): %s",
@@ -53,21 +54,13 @@ void	display_info(t_vm *vm)
 	mvwprintw(info, line + 1, 3, "Cycle to Die: %-4d", vm->cycle_to_die);
 	mvwprintw(info, line + 3, 3, "Cycle Delta: %d", CYCLE_DELTA);
 	mvwprintw(info, line + 5, 3, "Total_live: %d", vm->tot_lives);
-<<<<<<< HEAD
-	mvwprintw(info, line + 7, 3, "Carry: %d", vm->processes->carry);
-	mvwprintw(info, line + 9, 3, "r1: %x", vm->processes->r[0]);
-	mvwprintw(info, line + 10, 3, "r2: %x", vm->processes->r[1]);
-	mvwprintw(info, line + 11, 3, "r3: %x", vm->processes->r[2]);
-	mvwprintw(info, line + 12, 3, "r4: %x", vm->processes->r[3]);
-=======
 	/*mvwprintw(info, line + 8, 3, "Carry: %d", vm->processes->carry);
 	mvwprintw(info, line + 10, 3, "r1: %.8x", vm->processes->r[0]);
 	mvwprintw(info, line + 11, 3, "r2: %.8x", vm->processes->r[1]);
 	mvwprintw(info, line + 12, 3, "r3: %.8x", vm->processes->r[2]);
 	mvwprintw(info, line + 13, 3, "r4: %.8x", vm->processes->r[3]);*/
->>>>>>> master
 	wrefresh(info);
-	//display_players(vm, info);
+	display_players(vm, info);
 	delwin(info);
 }
 
