9ed2b4f6c0f89a6c45e7609d18dc7beeb1d5d2f2
Guilhem DE LA BROSSE <gdelabro@e3r4p11.42.fr>
2017-10-26 19:40:53 +0200

    dwq

--- corewar_dir/src/ncurses/display_players.c
+++ corewar_dir/src/ncurses/display_players.c
@@ -6,7 +6,7 @@
 /*   By: afourcad <afourcad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/19 18:23:29 by afourcad          #+#    #+#             */
-/*   Updated: 2017/10/26 19:31:12 by gdelabro         ###   ########.fr       */
+/*   Updated: 2017/10/26 19:40:37 by gdelabro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int		taille_nb(int nb)
 	int i;
 
 	i = 0;
-	t = nb == -2147483648 || !nb? 1 : 0;
+	t = nb == -2147483648 || !nb ? 1 : 0;
 	nb == -2147483648 ? nb = -214748364 : 0;
 	nb < 0 ? t += 1 : 0;
 	nb < 0 ? nb *= -1 : 0;
@@ -40,10 +40,10 @@ void	display_players(t_vm *vm, WINDOW *info, int *line)
 		mvwprintw(info, *line, 3, "Player (%d): ",
 				vm->players[players].number);
 		wattron(info, COLOR_PAIR(players + 2));
-		wattron(info, WA_ALTCHARSET);
+		wattron(info, WA_BOLD);
 		wprintw(info, "%.53s", vm->players[players].header.prog_name);
 		wattroff(info, COLOR_PAIR(players + 2));
-		wattroff(info, WA_ALTCHARSET);
+		wattroff(info, WA_BOLD);
 		mvwprintw(info, *line + 1, 5, "Last live %-10d",
 				vm->players[players].last_live);
 		mvwprintw(info, *line + 2, 5, "Number of live: %-10d",
