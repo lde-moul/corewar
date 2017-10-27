643c0da01457822020acbe8d73d57fe264abd5a3
Adrien FOURCADE <afourcad@e3r5p14.42.fr>
2017-10-27 20:25:43 +0200

    live change the winner

--- corewar_dir/src/instructions/live.c
+++ corewar_dir/src/instructions/live.c
@@ -6,7 +6,7 @@
 /*   By: afourcad <afourcad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/28 19:26:21 by afourcad          #+#    #+#             */
-/*   Updated: 2017/10/25 18:39:30 by afourcad         ###   ########.fr       */
+/*   Updated: 2017/10/27 20:20:34 by afourcad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	live(t_vm *vm, t_proc *proc, t_instruction *inst)
 	int			i;
 
 	i = 0;
-			proc->alive = 1;
-			vm->num_lives++;
-			vm->tot_lives++;
+	proc->alive = 1;
+	vm->num_lives++;
+	vm->tot_lives++;
 	while (i < vm->num_players)
 	{
 		if (vm->players[i].number == inst->params[0])
@@ -27,8 +27,12 @@ void	live(t_vm *vm, t_proc *proc, t_instruction *inst)
 			if (!vm->visu && vm->dump_cycle == -1)
 				ft_printf("The player \"%s\" ($%d) is in life !\n",
 					vm->players[i].header.prog_name, inst->params[0]);
+/*			else
+				mvprintw(60, 200, "The player \"%s\" ($%d) is in life !\n",
+					vm->players[i].header.prog_name, inst->params[0]);*/
 			++(vm->players[i].nb_live);
 			vm->players[i].last_live = vm->cycle;
+			vm->win = vm->players[i].number;
 			return ;
 		}
 		++i;
@@ -36,4 +40,8 @@ void	live(t_vm *vm, t_proc *proc, t_instruction *inst)
 	if (!vm->visu && vm->dump_cycle == -1)
 		ft_printf("The player \"UnknownPlayer\" ($%d) is in life !\n",
 			inst->params[0]);
+	else
+		mvprintw(60, 200, "The player \"%s\" ($%d) is in life !\n",
+			vm->players[i].header.prog_name, inst->params[0]);
+
 }
