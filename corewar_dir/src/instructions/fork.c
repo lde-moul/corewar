6e5318e02b1f6b89ad27d0110e814fb6453f4e7d
Louis-antoine DE MOULINS DE ROCHEFORT <lde-moul@e3r4p12.42.fr>
2017-10-31 17:50:37 +0100

    Same for lfork

--- corewar_dir/src/instructions/fork.c
+++ corewar_dir/src/instructions/fork.c
@@ -6,7 +6,7 @@
 /*   By: lde-moul <lde-moul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/17 16:20:24 by lde-moul          #+#    #+#             */
-/*   Updated: 2017/10/31 17:13:35 by lde-moul         ###   ########.fr       */
+/*   Updated: 2017/10/31 17:50:24 by lde-moul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	cor_fork(t_vm *vm, t_proc *proc, t_instruction *inst)
 void	cor_lfork(t_vm *vm, t_proc *proc, t_instruction *inst)
 {
 	create_process(vm, mod_adr(proc->pc + inst->params[0]), 0, 1);
+	vm->processes->alive = proc->alive;
 	memcpy(vm->processes->r, proc->r, REG_NUMBER * sizeof(int));
 	vm->processes->carry = proc->carry;
 }
